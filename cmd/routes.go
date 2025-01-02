af96c891355cba5ae8043cdf3cbcdd59fbf811bf
Talha Bay <ihsantalhabay@gmail.com>
2025-01-02 13:59:07 +0200

    cors updated

--- cmd/routes.go
+++ cmd/routes.go
@@ -28,18 +28,21 @@ func initRoutes(r *gin.Engine) {
 
 func CORSMw() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://kugulupark.netlify.app/")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://kugulupark.netlify.app")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT,DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+			c.AbortWithStatus(204) // No content
 			return
 		}
+
 		c.Next()
 	}
 }
+
+
 
 func IsAuthenticatedMW() gin.HandlerFunc {
 	return func(c *gin.Context) {
